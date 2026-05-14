1460604dd99b7633746aabcf4880b8b8b7b4d935
Joao Pedro da Silva Santos <joaobricia21@gmail.com>
2026-05-14 19:38:25 -0300

    Add files via upload

---
+++ aula 5.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main (void) {
+	
+    int vet [10];
+    vet[0] = 5;
+    vet[1] = 6;
+    vet[3] = 8;
+    vet[4] = 12;
+    vet[5] = 18;
+    vet[6] = 22;
+    vet[7] = 34;
+    vet [8] = 78;
+	vet[9] = 45;
+	
+	
+	
+	
+	
+	
+
+
+
+
+
+
+
+
+
+	return 0;
+}
